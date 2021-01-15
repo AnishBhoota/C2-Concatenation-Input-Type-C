302b7ea47a18a8e9bf2659e9066e855af2a3b8e6
Bhoota Anish <110155@eesd.org>
2021-01-15 00:17:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+## Concatenation
+
+`+` adding strings together, appending (add to the end)
+
+("Hello + World")
+"hello" + "world"  =  "helloworld"
+
+
+## Type cast
+Forcing  data type on a variable
+
+## input
+
+input is the way the computer gets information from the user
+pythnon initially treats input as __string__ type 
+Ex.
+5 = "5"
+
+### formula
+`variableName = input(Optional Prompt")`
+python stores the __input__ in the variableName
+
+
+### 
+To force an integer into string, `str(variableName)
+to force integer into string `int(variableName)
---
+++ main.py
@@ -0,0 +1,28 @@
+print("Hello" + " World")
+
+
+print("hello" + " world." + " Today" + " is" + " Thursday.")
+
+#If adding strings is called concatenations
+# what is + for integers
+# 5+4=9
+
+
+
+print ("5 + 4")
+print ( 5 + 4 )
+
+
+firstName = " Anish"
+print("My Name is" + firstName)
+age = ("12")
+print("My age is " + age)
+
+value = 10
+
+print("The value is " + str(10)) #I forced a value
+
+userName = input("What is your name? ")
+print("Hey" + userName)
+userBirth = input("when were you born?")
+print("Wow you are " + str(2021-int(userBirth)) + " years old!")
